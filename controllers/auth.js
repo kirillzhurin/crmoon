7625cc7b5f7d4a709149f72ba53509438278abea
Kirill Zhurin <kirillzhurin@gmail.com>
2019-08-06 13:40:01 +0300

    authentication user, added jwt

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,11 +1,35 @@
 const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
 const User = require('../models/User');
+const keys = require('../config/keys');
 
-module.exports.login = (req, res) => {
-  res.status(200).json({
-    email: req.body.email,
-    password: req.body.password
-  });
+module.exports.login = async (req, res) => {
+ const { email, password } = req.body;
+ try {
+  const candidate = await User.findOne({ email });
+  if (candidate) {
+    const passwordResult = bcrypt.compareSync(password, candidate.password);
+    if (passwordResult) {
+      const token = jwt.sign({
+        fullName: candidate.fullName,
+        email,
+        userId: candidate._id
+      }, keys.jwtKey, {expiresIn: 60 * 60 * 3})
+      res.status(200).json({ token })
+    } else {
+      res.status(401).json({
+        message: 'Invalid email or password'
+      });
+    }
+  } else {
+    res.status(404).json({
+      message: 'User is not found!'
+    });
+  }
+ } catch (error) {
+   console.log(error);
+ }
+ 
 };
 
 module.exports.register = async (req, res) => {
