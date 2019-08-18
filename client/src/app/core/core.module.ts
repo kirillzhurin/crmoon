ec47d36db4f2731d421eae2c9d3837b6375535a8
Kirill Zhurin <kirillzhurin@gmail.com>
2019-08-18 12:16:32 +0300

    updated core module

--- client/src/app/core/core.module.ts
+++ client/src/app/core/core.module.ts
@@ -1,8 +1,13 @@
 import { ModuleWithProviders, NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { HTTP_INTERCEPTORS, HttpErrorResponse } from '@angular/common/http';
+import { HTTP_INTERCEPTORS } from '@angular/common/http';
 import { NbPasswordAuthStrategy, NbAuthModule, NbAuthJWTToken, NbAuthJWTInterceptor } from '@nebular/auth';
+import { StoreModule } from '@ngrx/store';
+import { EffectsModule } from '@ngrx/effects';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
 
+import { environment } from '../../environments/environment' // Angular CLI environment
+import { reducers } from './store';
 
 const PROVIDERS = [
   ...NbAuthModule.forRoot({
@@ -34,7 +39,9 @@ const PROVIDERS = [
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
+    CommonModule,
+    StoreModule.forRoot(reducers),
+    StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
   ],
   exports: [
     NbAuthModule
