921115c32dbef8d58bbe548dba9d8ba2d6f1fc24
Kirill Zhurin <kirillzhurin@gmail.com>
2019-08-21 21:20:29 +0300

    added  ngx-progressbar for showing http request and route  process

--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -2,6 +2,9 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HttpClientModule } from '@angular/common/http'
+import { NgProgressModule } from '@ngx-progressbar/core';
+import { NgProgressHttpModule } from '@ngx-progressbar/http';
+import { NgProgressRouterModule } from '@ngx-progressbar/router';
 import { CoreModule} from './core/core.module'
 import { AppRoutingModule } from './app-routing.module';
 import { ThemeModule } from './theme/theme.module';
@@ -14,6 +17,13 @@ import { AppComponent } from './app.component';
     AppRoutingModule,
     HttpClientModule,
     BrowserAnimationsModule,
+    NgProgressModule.withConfig({
+      thick: true,
+      spinner: false,
+      color: '#0190fe',
+    }),
+    NgProgressRouterModule,
+    NgProgressHttpModule,
     CoreModule.forRoot(),
     ThemeModule.forRoot(),
   ],
