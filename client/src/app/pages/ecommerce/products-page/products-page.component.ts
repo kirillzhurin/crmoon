e8388da107fc293436431d0bbf2c95b26a3e0aa4
Kirill Zhurin <kirillzhurin@gmail.com>
2019-08-28 23:30:57 +0300

    auto selecting first category

--- client/src/app/pages/ecommerce/products-page/products-page.component.ts
+++ client/src/app/pages/ecommerce/products-page/products-page.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
 import { Router} from '@angular/router';
 import { Store, select} from '@ngrx/store';
 import { Observable, Subject, combineLatest, BehaviorSubject } from 'rxjs';
-import { switchMap, map, debounceTime, distinctUntilChanged } from 'rxjs/operators';
+import { switchMap, map, debounceTime, distinctUntilChanged, skipWhile, takeWhile } from 'rxjs/operators';
 import { RootState } from 'src/app/core/store';
 import Category from 'src/app/core/models/category';
 import Product from 'src/app/core/models/product';
@@ -14,12 +14,12 @@ import { selectAllCategories, LoadCategoriesAction, LOAD_CATEGORIES_SUCCESS } fr
   templateUrl: './products-page.component.html',
   styleUrls: ['./products-page.component.scss']
 })
-export class ProductsPageComponent implements OnInit {
+export class ProductsPageComponent implements OnInit, AfterViewInit {
   products$: Observable<Product[]>;
   categories$: Observable<Category[]>;
   loadingCategories$: Observable<boolean>;
   loadingProducts$: Observable<boolean>;
-  activeCategory$: Subject<Category> = new Subject();
+  activeCategory$: BehaviorSubject<Category|null> = new BehaviorSubject(null);
   searchTerm$: BehaviorSubject<string> = new BehaviorSubject('');
 
   constructor(private store: Store<RootState>, private router: Router) { }
@@ -27,13 +27,25 @@ export class ProductsPageComponent implements OnInit {
   ngOnInit() {
     this.store.dispatch(new LoadCategoriesAction());
     this.categories$ = this.store.select(selectAllCategories);
-    this.products$ = combineLatest(this.activeCategory$, this.searchTerm$.pipe(debounceTime(300), distinctUntilChanged())).pipe(
-      switchMap(([ category, term ]) => {
+
+    this.products$ = combineLatest(this.activeCategory$.pipe(skipWhile(activeCategory => !!activeCategory)), this.searchTerm$.pipe(debounceTime(300), distinctUntilChanged())).pipe(
+      switchMap(([ activeCategory, term ]) => {
         return this.store.pipe(
-          select(selectProductsByCategory, {categoryId: category._id}),
+          select(selectProductsByCategory, {categoryId: activeCategory._id}),
           map(products => products.filter(product => product.title.toUpperCase().indexOf(term.toUpperCase()) > -1)));
       })
     );
+  }
+
+  ngAfterViewInit() {
+    // Select first category
+    this.categories$.pipe(
+      takeWhile(categories => !categories.length, true)
+    ).subscribe(categories => {
+      if (categories.length) {
+        this.selectCategory(categories[0]);
+      }
+    });
   }
 
   onSearch(event) {
