47076c56b7e20fda31bda426cabc5758405690a0
Kirill Zhurin <kirillzhurin@gmail.com>
2019-08-29 22:37:51 +0300

    created invoice component

--- client/src/app/shared/shared.module.ts
+++ client/src/app/shared/shared.module.ts
@@ -1,10 +1,11 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { NgZorroAntdModule } from 'ng-zorro-antd';
+import { InvoiceComponent } from './components/invoice/invoice.component';
 import { ProductCardComponent } from './components/product-card/product-card.component';
 
 const DIRECTIVES = [];
-const COMPONENTS = [ProductCardComponent];
+const COMPONENTS = [ProductCardComponent, InvoiceComponent];
 
 @NgModule({
   declarations: [...COMPONENTS, ...DIRECTIVES],
