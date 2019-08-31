00410075212e1158dcc90abde3b26c3ca6812f27
Kirill Zhurin <kirillzhurin@gmail.com>
2019-08-31 23:56:00 +0300

    updated cart page

--- client/src/app/pages/ecommerce/cart-page/cart-page.component.ts
+++ client/src/app/pages/ecommerce/cart-page/cart-page.component.ts
@@ -1,8 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import { FormGroup, FormControl, ValidationErrors, Validators } from '@angular/forms';
 import { Store } from '@ngrx/store';
 import { Observable } from 'rxjs';
 import { RootState } from 'src/app/core/store';
-import { selectAllPositions, UpdatePositionAction, DeletePositionAction } from 'src/app/core/store/cart';
+import {
+  selectAllPositions,
+  UpdatePositionAction,
+  DeletePositionAction,
+  selectTotalPrice
+} from 'src/app/core/store/cart';
+import {
+  CreateOrderAction
+} from 'src/app/core/store/orders'
 import Position from 'src/app/core/models/position';
 declare var require: any
 const data: any = require('./data.json')
@@ -13,15 +22,35 @@ const data: any = require('./data.json')
   styleUrls: ['./cart-page.component.scss']
 })
 export class CartPageComponent implements OnInit {
+  form: FormGroup;
   ordersTableData = data.ordersTableData;
   invoicePrices = data.invoicePrices;
-  current = 0;
+  step = 0;
   positions$:Observable<Position[]>;
-
+  total$: Observable<number>;
+  invoiceData =  {
+    recipient: null,
+    total: 0,
+    positions: []
+  }
   constructor(private store: Store<RootState>) { }
 
   ngOnInit() {
     this.positions$ = this.store.select(selectAllPositions);
+    this.total$ = this.store.select(selectTotalPrice);
+
+    this.form = new FormGroup({
+      email: new FormControl('', [Validators.required, Validators.email]),
+      phone: new FormControl('', [Validators.required]),
+      name: new FormControl('', [Validators.required]),
+      surname: new FormControl('', [Validators.required]),
+      city: new FormControl('', [Validators.required]),
+      address: new FormControl('', [Validators.required]),
+      cardNumber: new FormControl('', [Validators.required]),
+      expirationDate: new FormControl('', [Validators.required]),
+      cvc: new FormControl('', [Validators.required]),
+      cardHolderName: new FormControl('', [Validators.required])
+    });
   }
 
   changeQuantity(position: Position) {
@@ -33,17 +62,23 @@ export class CartPageComponent implements OnInit {
   }
 
   pre(): void {
-    this.current -= 1
-    // this.changeContent();
+    this.step--
   }
 
   next(): void {
-    this.current += 1
-    // this.changeContent();
+    if (this.step == 1 && this.form.invalid) return;
+    this.step++
   }
 
   done(): void {
-    console.log('done')
+    //this.store.dispatch(CreateOrderAction())
   }
 
+  getInvoiceData(positions, total) {
+    this.invoiceData.recipient = this.form.value;
+    this.invoiceData.positions = positions
+    this.invoiceData.total = total;
+    return this.invoiceData;
+
+  }
 }
--- client/src/app/shared/components/invoice/invoice.component.ts
+++ client/src/app/shared/components/invoice/invoice.component.ts
@@ -1,12 +1,23 @@
-import { Component, OnInit } from '@angular/core'
-declare var require: any
-const invoiceData: any = require('./data.json')
+import { Component, OnInit, Input } from '@angular/core';
+import { format, addDays } from 'date-fns';
+import Position from 'src/app/core/models/position';
+
 @Component({
   selector: 'invoice',
   templateUrl: './invoice.component.html',
 })
 export class InvoiceComponent implements OnInit {
-  data = invoiceData.invoiceData
+  senderName = "Amazon Delivery";
+  senderLogo = 'assets/images/amazon.jpg';
+  senderAdress = '795 Folsom Ave, Suite 600, San Francisco, CA, 94107';
+  senderEmail = 'example@amazon.com';
+  senderPhone = '(123) 456-7890';
+  senderFax = '800-692-7753';
+  invoiceNumber = `CRM${(~~(Math.random() * 1e8)).toString(16)}`;
+  invoiceDueDate = format(new Date(), 'MM dd, yyyy')
+  invoiceDate = format(addDays(new Date(), 5), 'MM dd, yyyy')
+
+  @Input() data: any = {}
   constructor() {}
   ngOnInit() {}
 }
